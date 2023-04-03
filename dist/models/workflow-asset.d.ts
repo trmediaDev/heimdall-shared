83114e6d8ed7d2a890b2643e47dd671f2d500875
Md. Abdur Rouf <official.rouf69nb@gmail.com>
2023-04-03 16:30:12 +0600

    added 'createdDate' to  WorkflowAsset class

--- dist/models/workflow-asset.d.ts
+++ dist/models/workflow-asset.d.ts
@@ -1,6 +1,7 @@
 import { ObjectId } from 'bson';
 import { UploadAssetType } from './upload-asset-type';
 export declare class WorkflowAsset {
+    _id: ObjectId;
     type: UploadAssetType;
-    assetId: ObjectId;
+    createdDate: Date;
 }
--- src/models/workflow-asset.ts
+++ src/models/workflow-asset.ts
@@ -2,6 +2,7 @@ import { ObjectId } from 'bson';
 import { UploadAssetType } from './upload-asset-type';
 
 export class WorkflowAsset {
+    _id: ObjectId;
     type: UploadAssetType;
-    assetId: ObjectId;
+    createdDate: Date;
 }
