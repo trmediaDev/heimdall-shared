c4910f8631c16c5e6218210f1b28e0318bd2519e
shofiul <its.shofiul@gmail.com>
2024-01-10 14:25:47 +0600

    export fixed

---
+++ dist/models/asset.variants.d.ts
@@ -0,0 +1,20 @@
+export declare class ImageVariantsObject {
+    filename: string;
+    key: string;
+    width: number;
+    height: number;
+}
+export declare class Variants {
+    thumb_xs?: ImageVariantsObject;
+    thumb?: ImageVariantsObject;
+    xs?: ImageVariantsObject;
+    s?: ImageVariantsObject;
+    s2?: ImageVariantsObject;
+    s3?: ImageVariantsObject;
+    s4?: ImageVariantsObject;
+    s5?: ImageVariantsObject;
+}
+export declare const imageVariantOptions: {
+    name: string;
+    width: number;
+}[];
